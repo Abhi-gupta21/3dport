d1046b372abd07331c6db2669d46279abe393c25
SAI ABHISHEK KETEPALLY <ketepask@mail.uc.edu>
2024-10-27 23:03:55 -0300

    idk

--- vite-react/vite.config.js
+++ vite-react/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/Abhi-gupta21.github.io/', // Use your repository name here
+  root: 'threedport',
 })
